d9f59b94a112e6a7d4438e5d1cf9ca7e484b25dc
AyushGurg <ayshgrg@gmail.com>
2023-03-23 12:34:06 -0600

    Learn function of webpacks

---
+++ src/index.js
@@ -0,0 +1,14 @@
+let header = document.createElement("div");
+header.id = "header";
+document.body.appendChild(header);
+
+let home = document.createElement("div");
+home.id = "home";
+let menu = document.createElement("div");
+menu.id = "menu";
+let contact = document.createElement("div");
+contact.id = "contact";
+
+header.appendChild(home);
+header.appendChild(menu);
+header.appendChild(contact);
