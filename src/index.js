2e648d557cd1911f66c58b4cd5d2add9a51f4d0b
AyushGurg <ayshgrg@gmail.com>
2023-04-07 20:18:07 -0600

    Finish the project (barebones)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,59 @@
 import { header } from "./header.js";
+import { main, removemain } from "./mainpage.js";
+import { menu, removemenu } from "./menu.js";
+import { contact, removecontact } from "./contact.js";
 
 header();
+main();
+
+let homeButton = document.getElementById("home");
+let menuButton = document.getElementById("menu");
+let contactButton = document.getElementById("contact");
+
+let current = "main";
+
+homeButton.addEventListener("click", () => {
+  switch (current) {
+    case "menu":
+      removemenu();
+      current = "main";
+      break;
+    case "contact":
+      removecontact();
+      current = "main";
+      break;
+    default:
+      break;
+  }
+  main();
+});
+menuButton.addEventListener("click", () => {
+  switch (current) {
+    case "main":
+      removemain();
+      current = "menu";
+      break;
+    case "contact":
+      removecontact();
+      current = "menu";
+      break;
+    default:
+      break;
+  }
+  menu();
+});
+contactButton.addEventListener("click", () => {
+  switch (current) {
+    case "main":
+      removemain();
+      current = "contact";
+      break;
+    case "menu":
+      removecontact();
+      current = "contact";
+      break;
+    default:
+      break;
+  }
+  contact();
+});
